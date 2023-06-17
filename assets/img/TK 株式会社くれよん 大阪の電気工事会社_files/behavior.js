e8bdbf9de3329c14fc142401f9276c11d78ebe81
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-06-17 16:12:08 +0900

    imageだけ拡大

---
+++ assets/img/TK 株式会社くれよん 大阪の電気工事会社_files/behavior.js
@@ -0,0 +1,20 @@
+////////////
+// 属性『letter-spacing: .5em;』を最後の文字だけ取り去る
+function killLetterSpace(arr) {
+  arr.forEach(elem => {
+    let lastChar = elem.textContent.slice(-1)
+    let preText = elem.textContent.slice(0, -1)
+    elem.innerHTML = `${ preText }<span class='remove-letter-spacing'>${ lastChar }</span>`
+  })
+}
+
+const arg = document.querySelectorAll('.lead-copy')
+// const arg = Array.from(document.querySelectorAll('.lead-copy'))
+// const arg2 = Array.from(document.querySelectorAll('.more-info-btn'))
+// const arg = arg1.concat(arg2)
+killLetterSpace(arg)
+
+
+
+////////////
+// 
